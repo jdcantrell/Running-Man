5c6a6ecf0aa4934536028e76238ebb90f516cc75
JD Cantrell <jdcantrell@gmail.com>
2012-03-13 21:41:31 -0700

    Add initial data set

---
+++ data.py
@@ -0,0 +1,21 @@
+'''
+This is a list of run data that I have gathered
+
+Format:
+  date, 24hr time, distance, run_time, max_speed, min_speed, feeling, food
+
+date - YYYY-MM-DD
+time - hr:min
+distance - miles
+time - hr: min
+max_speed - mph
+min_speed - mph
+feeling - bad, okay, good
+mood - angry, sad, happy, upset, anxious, etc
+food - list of foods eaten prior to run (the meal before), including liquids
+'''
+
+runs = [
+  ("2012-03-11", "19:30", 1.25, "25:00", 5.0, 2.9, "okay", "happy", ["rice", "pork", "cheese"]),
+  ("2012-03-13", "20:30", 1.88, "25:00", 6.6, 3.6, "good", "happy", ["soda", "pasta", "water"]),
+]
